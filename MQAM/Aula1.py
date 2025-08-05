feaa84e7e9381e566ba9fe736b09f8518aad9749
Higor <higor.freitas@usp.br>
2025-08-05 13:49:18 -0300

    MQAM

---
+++ MQAM/Aula1.py
@@ -0,0 +1,33 @@
+#Aula 1
+#Professor Laureto
+    #Especializado muito em IA e em Machine Learning
+    #Formato do arquivo em Py pois na prática ele usará essa linguagem
+    #Não tão proeficiente em estatistica, então nem tudo será cobrido
+
+#Avaliação (Cabível mudanças)
+    #Estudos de caso durante o semestre (3 ou mais)
+    #Grupos de 2 ou 3 alunos
+
+#Pandas será útil para avaliar os modelos
+
+#Haverá contas na matéria, mas usarei o notebook como anotação principal
+
+
+
+
+#Regressão linear simples:
+    #Conceito básico da primeira aula
+    #Relação de duas varíaveis diferentes, uma X e Y
+    #X na abscissa, representado a preditora e Y na ordenada, representando a resposta
+    #Objetivo é tentar prever Y com base em X
+    #É simples pois nesse caso há apenas uma variável preditora 
+    #A relação será uma "Função linear" entre X e Y (Y = aX + b). A é beta 1 e b é beta 0 na literatura
+    #Ocorrerá espalhamento, só será de fato uma linha reta caso seja relação deterministca, o que não faz sentido aplicação
+
+    #Encontrar a linha
+    #Sempre estará aproximadaemnte no meio dos pontos, existirá um erro para cada Yi, Ei = Yi - Ŷi
+        #Objetivo é minimizar o erro, ou seja, minimizar a soma dos erros
+        #Q = Σ(Ei)^2 = Σ(Yi - Ŷi)^2
+        #O Ŷ é o que foi estimado, pelo que entendi pode ser uma média ou uma outra aproximação feita
+
+
