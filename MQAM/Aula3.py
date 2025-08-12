6784b6c20bf746f94bfc930c9a877fcb572f5ad9
Higor <higor.freitas@usp.br>
2025-08-11 22:20:59 -0300

    MQAM

---
+++ MQAM/Aula3.py
@@ -0,0 +1,46 @@
+#Aula 3, não estou entendendo a mateŕia exatamente do que se trata
+
+
+
+#Variacia amostral tem o n-1 devido aos graus de liberdade
+#Lembrando que encontar B0 e B1 é feito através de derivadas
+    
+
+#Y barra é a média, mas normelmente é uma aproximação, uma predição. Ela nesse caso não depende de modelo, é uma média geral que não depende de X
+    #STTO então é (yi-Ybarra)², a distancai do ponto até a média dele
+    #SSR que é basicamente á distancia quadrática  total do que eu previ pela regressão até a média
+    #SSE é a distancia quadrática do ponto até o que eu previ da regressão
+
+    #Por isso SSTO
+
+#O objetivo então é fazer que a distancia total seja formada principalmente formada pela distancai da média, logo os erros do modelo são justificados pela variância do modelo em sí.
+
+#Ou seja o R² é SSR / SSTO, ou seja quanto maior melhor, mais SSR é mais "erro por conta da variancia" e não do modelo
+
+#Da para interpretar que R²% da varição Y é explciada pela variação do preditor X
+#Ou R² da incerteza do Y é reduzida pela variação do preditor X
+
+#Cuidado com relações causais, isso é um engano classico da regressão
+
+
+#Coeficiente de correlação com Pearson é a raiz do R², chamada obivamente de r
+    #Sinal vai corresponder exatamente pelo slope (Coeficiente b1)
+    #Modelos de regressão múltipla normalmente utilizam R² para classificação
+
+
+
+#Modelo pode ser adaptado dependendo da proporção à priori, ex y => x², então a regressão seria uma parabola
+    #Ajustes de escala clarificam isso graficiaemnte, para vizualização como linha
+
+#lembrando que R² alto não necessariamente implica que o modelo é bom, do mesmo jeito um R² não indica que o B1 é alto ou baixo.
+
+#Experimento - Descrição que enm a de Introdução à Estatistica
+    #Grupo controle, diferença de diferença, comparação etc...
+
+
+#1.9 Teste de hipotese sobre cofieicnete de correlação foi pulado por enquanto
+
+
+
+
+    #
