9510d2d7d6b52aae02b25cb8697029743727b801
Higor <higor.freitas@usp.br>
2025-09-11 00:10:10 -0300

    mqam

---
+++ OAC II/Aula6.asm
@@ -0,0 +1,41 @@
+#A melhor aula numero 6 de oacII:
+
+#Começo de aula em saídas
+
+#A partir de agora a definição da instruções é MIPS 
+    #Dicussão na literatura da maneira de usa-lo
+
+
+#Considerações do MIPS
+    #32 Registradores apenas
+        #Compilador do programa que realiza essa associação
+    #Memoria de 32 Bits
+    #Palavras de 4 bytes
+
+#Tem a convenção dos 32 registradores, já vista em OAC I
+
+
+
+
+#Programa vai para o compilador que solta um assembly
+#assembly vai para um montador que gera codigo de maquina
+#Codigo de máquina junto com uma biblioteca de rotinas (como DLLs no windows) vai para o link editor que trasnforma em executável
+#Executavel vai par ao loader que entra na memoria
+
+
+#Tipos de comandos são R I e J, cada um com especificações
+#Jump tem sempre endereços realtivos, pois não caberia na memoria diretamente.
+
+
+
+#ULA calcula operações, 32 bits em cada entrada e uma saída de 32 + outras saída de "Desce zero"
+
+#PC é um registrador travado além dos 32 que me diz onde está a proxima instrução
+
+
+#Borad de subida e descida tem qu estar bem definido
+
+
+#Tem varias unidades funcionais, mais tudo isso já foi visto em OAC I, a tabela que representa um computador.
+
+#Tem o monociclo e o multiciclo
