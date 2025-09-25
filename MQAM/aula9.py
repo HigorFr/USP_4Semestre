5ab269fd313034d552e223add5b477f0dae1ba2f
Higor <higor.freitas@usp.br>
2025-09-24 22:33:29 -0300

    BD I

---
+++ MQAM/aula9.py
@@ -0,0 +1,8 @@
+#Explicação de porque intervalos de confiança são diferentes de intervalos de predição
+    #Intervalo de confiança é sobre a média, ou seja, se eu pegar várias amostras, a média delas vai cair nesse intervalo
+        #Intervalo de predição é sobre um novo valor, ou seja, se eu pegar uma nova amostra, o valor dela vai cair
+            #Intervalo de predição é sempre maior que o de confiança, pois ele tem que englobar a variabilidade dos dados, e não só a média
+
+
+
+
